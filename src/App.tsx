a20caa24beae675ae7e1850796256a9a755c0613
LGRosi <lrosi1996@gmail.com>
2022-10-24 08:12:24 -0300

    Avance de la lógica para publicar comentario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import './App.scss'
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
 
+    const [comment, setComment] = useState<string>('');
+    const [comments, setComments] = useState<string[]>([]);
+
+    const handleClick = () => {
+        setComments(comments => [...comments, comment]);
+    } 
+
+    const handleChange = (e: any) => {
+        setComment(e.target.value);
+    }
+
+
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
-  )
+    <React.Fragment>
+      <div className="main-container">
+        {
+            comments.map((text) => (
+                <div key={text} className="comments-container">{text}</div>
+            ))
+        }
+
+        <div className="comments-box-container">
+          <h1 className="comment-title">Comentarios</h1>
+          <textarea
+            className="comment-textarea"
+            onChange={handleChange}
+            value={comment}
+          ></textarea>
+          <button
+            type="button"
+            className="comment-button"
+            onClick={handleClick}
+          >
+            Publicar
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
