62b5e5c307fe5a0f3cec00ad50cf1f746e883443
LGRosi <lrosi1996@gmail.com>
2022-11-12 20:42:37 -0300

    Se creo un componente para el NavLink personalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
-const Home = () => <h1>Home</h1>
 
 const SearchPage = () => <h1>Search Page</h1>
 
 function App() {
-
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search-page" element={<SearchPage />} />
-      </Routes>
-    </div>
+    <>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search-page" element={<SearchPage />} />
+            <Route path="*" element={<Home />} />
+        </Routes>
+    </>
   );
 }
 
