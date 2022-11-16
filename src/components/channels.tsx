b8958715659f80f0ce4ed1b323a651faca6896e0
LGRosi <lrosi1996@gmail.com>
2022-11-16 17:22:38 -0300

    Armado de la pagina de canales de ideas

--- src/components/channels.tsx
+++ src/components/channels.tsx
@@ -1,9 +1,52 @@
+import { Channel } from "../models/channels.interfaces";
 import "../scss/components/_channels.scss";
+import { BsHash } from "react-icons/bs";
+import { useState } from "react";
 
 function Channels() {
+
+    // const [channels, setChannels] = useState([]);
+
+    const arrayChannels: Channel[] = [
+        {
+            id: 1,
+            icon: BsHash,
+            title: 'Educación'
+        },
+        {
+            id: 2,
+            icon: BsHash,
+            title: 'Salud'
+        },
+        {
+            id: 3,
+            icon: BsHash,
+            title: 'Donaciones'
+        },
+        {
+            id: 4,
+            icon: BsHash,
+            title: 'Transporte Público'
+        },
+    ];
+
     return(
-        <div className="channel-container">
-            
+        <div className="div-channel-container">
+            <p className="p-title">Canales de ideas:</p>
+            <ul className="ul-channel-container">
+                {
+                    arrayChannels.map(channelItems => (
+                        <li key={channelItems.id} className="li-channel-element">
+                            <button type="button">
+                                <channelItems.icon color="#4e42d4" size={25} />
+                                <p className="p-title-element">
+                                    { channelItems.title }
+                                </p>
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
---
+++ src/models/channels.interfaces.ts
@@ -0,0 +1,7 @@
+import { IconType } from 'react-icons';
+
+export interface Channel {
+    id: number;
+    icon: IconType;
+    title: string;
+}
