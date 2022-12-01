477ac21fadf9ade845ca60623cdfe5c8efa17ddf
LGRosi <lrosi1996@gmail.com>
2022-11-30 21:14:32 -0300

    Estilos para el logout

--- src/components/SideBarMenu.tsx
+++ src/components/SideBarMenu.tsx
@@ -5,6 +5,7 @@ import { VscMenu } from "react-icons/vsc";
 import SideBarMenuCardView from "./SideBarMenuCardView";
 import SideBarMenuItemsView from "./SideBarMenuItemsView";
 import "../scss/layouts/_side-bar-menu.scss";
+import { CgLogOut } from "react-icons/cg";
 
 interface SideBarMenuProps {
     items: SideBarMenuItem[];
@@ -12,26 +13,42 @@ interface SideBarMenuProps {
 }
 
 export function SideBarMenu({ items, card }: SideBarMenuProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
 
-    const [isOpen, setIsOpen] = useState<boolean>(true);
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
 
-    function handleClick() {
-        setIsOpen(!isOpen);
-    }
+  function onLogout() {
+    localStorage.removeItem("token");
+    //   setIsAuthenticated(false);
+  }
 
-  return(
-    <div className={classNames('side-bar-menu', isOpen ? 'expanded' : 'collapsed')}>
-        <div className="menu-button">
-            <button className="hamburger-button" type="button" onClick={handleClick}>
+  return (
+    <div
+      className={classNames("side-bar-menu", isOpen ? "expanded" : "collapsed")}
+    >
+        <div className="navigate-container">
+            <div className="menu-button">
+                <button
+                className="hamburger-button"
+                type="button"
+                onClick={handleClick}
+                >
                 <VscMenu size="22" />
+                </button>
+            </div>
+            <SideBarMenuCardView card={card} isOpen={isOpen} />
+            {items.map((item) => (
+                <SideBarMenuItemsView key={item.id} item={item} isOpen={isOpen} />
+            ))}
+        </div>
+
+        <div className="logout-container">
+            <button type="submit" onClick={onLogout}>
+            <CgLogOut size={25} color="white" />
             </button>
         </div>
-        <SideBarMenuCardView card={card} isOpen={isOpen} />
-        {
-            items.map(item => (
-                <SideBarMenuItemsView key={item.id} item={item} isOpen={isOpen} />
-            ))
-        }
     </div>
   );
 }
